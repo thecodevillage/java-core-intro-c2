ac10e3fd3b85cb5c64e838d8b09f46e06d888f73
li <li@Administrators-MacBook-Pro-3.local>
2020-06-30 19:33:20 +0300

    My Initiaal Commit

---
+++ java/day1/hello.java
@@ -0,0 +1,9 @@
+
+//master syntax
+//aware of your code
+class Hello {
+  public static void main(String[] args){
+    // this outputs Hello World on the screen
+    System.out.println("Hello Nairobi"); //statements end in semi colons
+  }
+}
---
+++ java/day2/sum.java
@@ -0,0 +1,9 @@
+
+class Sum{
+public static void main(String[] args) {
+  int a = 20;
+  int b = 30;
+  int sum = a + b;
+  System.out.println("The sum is ##"+sum);
+}
+}
